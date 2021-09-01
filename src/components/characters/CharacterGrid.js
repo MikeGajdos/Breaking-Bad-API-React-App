0a4faf800444b3ceb0101154db91622f873a7ba5
Mike Gajdos <mikegajdos81@gmail.com>
2021-09-01 16:31:07 +0100

    Modified ReadMe

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -2,6 +2,7 @@ import React from "react";
 import CharacterItem from "./CharacterItem";
 import Spinner from "../UI/Spinner";
 
+console.log("what the fuck");
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
     <Spinner />
